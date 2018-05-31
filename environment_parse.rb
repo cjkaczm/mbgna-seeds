1892fba8e18ff2ac400d2b2799b5c6b6df8eeb24
cjkaczm <cjkaczma@umich.edu>
2018-05-31 12:52:25 -0400

    Add initial native seed files and parser
    Each spreadsheet contains the species found in a certain environment. They were generated with the all_species csv and Ruby parsing script.

---
+++ environment_parse.rb
@@ -0,0 +1,63 @@
+#native seed parser
+#cjkaczma
+#2018.5.24
+#Read the native seeds list and export seperate .csv's for each environment
+
+require 'C:\Ruby24-x64\lib\ruby\2.4.0\csv.rb'
+
+ALL_FILEPATH = 'all_species.csv'
+ENVIRONMENTS = [
+  'woods',
+  'dappled woods',
+  'wetland sun',
+  'wetland shade',
+  'riparian',
+  'prairie',
+  'alvar',
+  'dune'
+]
+
+all_species_csv = CSV.read(ALL_FILEPATH)
+
+ENVIRONMENTS.length.times do |i|
+  puts ENVIRONMENTS[i]
+	#Reads through each line of the "all" file and writes the line to the output
+	#if the environment is checked
+  current_output = CSV.open(ENVIRONMENTS[i] + ".csv", "w")
+  #Writes header
+  current_output << [
+                    'Scientific Name',
+                    'Common Name',
+                    'Apr',
+                    'May',
+                    'Jun',
+                    'Jul',
+                    'Aug',
+                    'Sep',
+                    'Oct',
+                    'Nov',
+                    'Special Concern or Threatened',
+                    'Coeff of Concern'
+                    ]
+
+	all_species_csv.each do |species|
+		if species[0] == 'Scientific Name' #Hack to skip the headers
+		elsif species[i + 2]
+			current_output << [
+                        species[0], #Scientific name
+                        species[1], #Common name
+                        species[10], #Apr
+                        species[11], #May
+                        species[12], #Jun
+                        species[13], #Jul
+                        species[14], #Aug
+                        species[15], #Sep
+                        species[16], #Oct
+                        species[17], #Nov
+                        species[18], #SC/T
+                        species[19], #Coeff of concern
+                      ]
+		end
+	end
+  current_output.close
+end
